6be6131f099c142b1e0bc11960541ff436852b9a
Sonampriyadarshani <140054742+Sonampriyadarshani@users.noreply.github.com>
2025-03-20 14:28:15 +0530

    Update Store.js

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import formReducer from "./formSlice";  // Existing form slice
-import authReducer from "./authSlice";  // Existing auth slice
-import dataReducer from "./dataSlice";  // Add your dataSlice here
+import formReducer from "./formSlice";  
+import authReducer from "./authSlice";  
+import dataReducer from "./dataSlice";  
 
 const store = configureStore({
   reducer: {
     form: formReducer,  // Form-related state
     auth: authReducer,  // Authentication-related state
-    data: dataReducer,  // Data-related state (fetching posts or other data)
+    data: dataReducer,  // Data-related state 
   },
 });
 
